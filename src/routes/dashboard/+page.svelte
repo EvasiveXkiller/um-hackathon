58b150edf307acbb504bd9448c8a69b7c3bc0fab
evasivexkiller <evasivexkiller@gmail.com>
2023-05-05 20:15:44 +0800

    task system barebones in place

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,9 +3,11 @@
 		Form,
 		FormGroup,
 		Checkbox,
-		Button, PasswordInput, TextInput, Link,
+		Button, PasswordInput, TextInput, Link, SelectableTile, Modal,
 	} from "carbon-components-svelte";
 	import Spacer from "$lib/components/Spacer.svelte";
+	let open = false;
+	let isSelected = false;
 </script>
 
 <container class="w-screen grid place-items-center">
@@ -24,6 +26,28 @@
 			<Button href="/dashboard/update-health">Update your situation on the living thing in your tummy</Button>
 
 			<Button href="/dashboard/shop">Shop</Button>
+
+			<h1>Tasks</h1>
+
+			<div role="group" aria-label="selectable tiles">
+				<SelectableTile disabled={isSelected} on:select={() => (open = true)}>Task 1</SelectableTile>
+				<SelectableTile selected>Task 2</SelectableTile>
+				<SelectableTile>Task 3</SelectableTile>
+			</div>
+
+			<Modal
+					bind:open
+					modalHeading="Create database"
+					primaryButtonText="Confirm"
+					secondaryButtonText="Cancel"
+					on:click:button--secondary={() => (open = false)}
+					on:open={() => {isSelected = false}}
+					on:close={() => {isSelected = false}}
+					on:submit
+			>
+				<p>Create a new Cloudant database in the US South region.</p>
+			</Modal>
+
 		</div>
 	</div>
 	<Spacer height="64px"/>
