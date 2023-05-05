9f045c45c38ea6f529ff12cf7190572fbc1ae384
evasivexkiller <evasivexkiller@gmail.com>
2023-05-05 21:26:13 +0800

    shop placeholder now works properly

--- src/routes/dashboard/shop/+page.svelte
+++ src/routes/dashboard/shop/+page.svelte
@@ -22,15 +22,13 @@
 						subtitle="You dont have enough funds to buy"
 				/>
 			{/if}
-			<div class="grid grid-cols-4 gap-4">
+			<div class="flex flex-col gap-6">
 				{#each [1, 2, 3, 4, 5, 6, 6, 7, 7] as command}
-					<div class="">
-						<Tile on:click={() => (open = true)} class="">
-							<div class="grid place-items-center items-center">
-								<div>{command}</div>
-							</div>
-						</Tile>
-					</div>
+					<Tile on:click={() => (open = true)} class="">
+						<div class="grid place-items-center items-center">
+							<div>{command}</div>
+						</div>
+					</Tile>
 
 				{/each}
 			</div>
