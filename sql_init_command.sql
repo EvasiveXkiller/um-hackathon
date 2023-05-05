e25451e50dcdfbbbbf22d7acd9510e94d1e71c62
evasivexkiller <evasivexkiller@gmail.com>
2023-05-05 20:37:14 +0800

    updated sql code to match new rough system

--- sql_init_command.sql
+++ sql_init_command.sql
@@ -3,6 +3,11 @@ create table main.users
     id            text not null,
     email         text not null,
     password      text not null,
-    userAuthToken text not null
+    userAuthToken text not null,
+    coins         integer default 100,
+    age           integer,
+    height        text,
+    weight        text,
+    diseases      text
 );
 
