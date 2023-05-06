76205c97a0ab351694eee96793ce6d2d040ee0e0
evasivexkiller <evasivexkiller@gmail.com>
2023-05-06 20:31:31 +0800

    Changed shop rendering structure

--- src/routes/dashboard/shop/+page.svelte
+++ src/routes/dashboard/shop/+page.svelte
@@ -9,16 +9,23 @@
 	import piano from '$lib/assets/image 16.png'
 	import pen from '$lib/assets/image 17.png'
 
-
 	let open = false;
+	let currentlySelected = false;
 	export let form = {};
 
-	const images = [piano, pen];
-	const descriptions = [
-		'With piano skills, your child would be able to play music pieces for you!',
-		'Learning how to write could be tough, but with patience and guidance, your child would love to write!'
+	const tilesData = [
+		{
+			title: "Piano",
+			description: "With piano skills, your child would be able to play music pieces for you!",
+			image: piano
+		},
+		{
+			title: "Writing",
+			description: "Learning how to write could be tough, but with patience and guidance, your child would love to write!",
+			image: pen
+		}
 	]
-	const titles = ["Piano", "Writing"];
+
 </script>
 
 <svelte:head>
@@ -38,41 +45,44 @@
 				/>
 			{/if}
 			<div class="flex flex-col gap-3">
-				{#each images as image, index}
-					<Tile on:click={() => (open = true)} class="">
+				{#each tilesData as iData}
+					<Tile on:click={() => {
+						open = true;
+						currentlySelected = iData
+					}} class="">
 						<div class="flex flex-row items-start">
 							<div class="flex flex-col mr-4">
-								<h3 class="text-2x1 font-bold mr-4">{titles[index]}</h3>
-								<div>{descriptions[index]}</div>
+								<h3 class="text-2x1 font-bold mr-4">{iData.title}</h3>
+								<div>{iData.description}</div>
 								<button class="mt-4 bg-purple-500 text-white px-4 py-2 rounded-md">Buy</button>
 							</div>
-							<div class="w-full"><img src={image} alt={'Image ${index + 1}'} class="w-full"></div>
+							<div class="w-full"><img src={iData.image} alt={`${iData.title}`} class="w-full"></div>
 						</div>
-
-
 					</Tile>
-
 				{/each}
-
 			</div>
-
-
 			<Modal
 					bind:open
-					modalHeading="Create database"
-					primaryButtonText="Confirm"
-					secondaryButtonText="Cancel"
-					on:click:button--secondary={() => (open = false)}
+					modalHeading="Buy {currentlySelected.title} skill?"
+					primaryButtonText="Cancel"
+					on:click:button--primary={() => (open = false)}
 					on:open
 					on:close
-					on:submit
 			>
-				<Form method="POST">
-					<input type="hidden" name="cost" value="100"/>
-					<Button type="submit" kind="primary">Buy</Button>
-				</Form>
+				<div>
+					<p style="text-align: left">
+						{currentlySelected.description}
+						<InlineNotification kind="info" style="width: 100%">This action would cost 100 coins</InlineNotification>
+					</p>
+					<ImageLoader src="{currentlySelected.image}"/>
+					<Spacer/>
+					<Form method="POST">
+						<input type="hidden" name="cost" value="100"/>
+						<Button type="submit" kind="primary">Buy</Button>
+					</Form>
+					<Spacer/>
+				</div>
 			</Modal>
-
 		</div>
 	</div>
 	<Spacer height="64px"/>
